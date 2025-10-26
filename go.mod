4d1143b21653322b3f32e625a8ebf24243203f8b
Ayadil <ayadil005@mail.ru>
2025-10-26 10:25:00 +0500

    Add backend skeleton and module structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module food-store-backend
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Food Store Backend Initialized"))
+	})
+
+	log.Println("Server running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
