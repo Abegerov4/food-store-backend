390261d8b4f1ff5b5db1f22d5c8f2c170e35bf56
Ayadil <ayadil005@mail.ru>
2025-10-26 10:25:00 +0500

    feat: add core domain models

--- models/food.go
+++ models/food.go
@@ -1,1 +1,7 @@
 package models
+
+type Food struct {
+	ID    string  `json:"id"`
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
--- models/order.go
+++ models/order.go
@@ -1,1 +1,8 @@
 package models
+
+type Order struct {
+	ID      string   `json:"id"`
+	FoodIDs []string `json:"food_ids"`
+	Total   float64  `json:"total"`
+	Status  string   `json:"status"`
+}
