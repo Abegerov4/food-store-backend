1101cfbb7215962d5997e51bb16744ca172acb4b
Ayadil <ayadil005@mail.ru>
2025-10-26 10:25:00 +0500

    feat: implement thread-safe in-memory repositories

--- repositories/food_repository.go
+++ repositories/food_repository.go
@@ -1,1 +1,56 @@
 package repositories
+
+import (
+	"sync"
+
+	"food-store-backend/models"
+)
+
+type FoodRepository struct {
+	mu    sync.Mutex
+	foods map[string]models.Food
+}
+
+func NewFoodRepository() *FoodRepository {
+	return &FoodRepository{foods: make(map[string]models.Food)}
+}
+
+func (r *FoodRepository) Create(food models.Food) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.foods[food.ID] = food
+}
+
+func (r *FoodRepository) GetAll() []models.Food {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	result := []models.Food{}
+	for _, f := range r.foods {
+		result = append(result, f)
+	}
+	return result
+}
+
+func (r *FoodRepository) GetByID(id string) (models.Food, bool) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	food, ok := r.foods[id]
+	return food, ok
+}
+
+func (r *FoodRepository) Update(id string, food models.Food) bool {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	if _, ok := r.foods[id]; !ok {
+		return false
+	}
+	r.foods[id] = food
+	return true
+}
+
+func (r *FoodRepository) Delete(id string) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	delete(r.foods, id)
+}
--- repositories/user_repository.go
+++ repositories/user_repository.go
@@ -1,1 +1,32 @@
 package repositories
+
+import (
+	"sync"
+
+	"food-store-backend/models"
+)
+
+type UserRepository struct {
+	mu    sync.Mutex
+	users map[string]models.User
+}
+
+func NewUserRepository() *UserRepository {
+	return &UserRepository{users: make(map[string]models.User)}
+}
+
+func (r *UserRepository) Create(user models.User) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.users[user.ID] = user
+}
+
+func (r *UserRepository) GetAll() []models.User {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	result := []models.User{}
+	for _, u := range r.users {
+		result = append(result, u)
+	}
+	return result
+}
